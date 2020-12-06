2c84e8c4e6280982c62995ecb1105caa9e7cb25e
adyang94 <adyang94@yahoo.com>
2020-12-06 13:23:17 -0600

    updates

--- library/library.js
+++ library/library.js
@@ -1,4 +1,25 @@
 //CONSTANTS AND VARIABLES----------------------------------------
-
+let myLibrary = [];
 //FUNCTIONS------------------------------------------------------
+function book() {
+
+};
+function addBookToLibrary () {
+    
+};
+function displayBooks () {
+    //Will need some example books to display.
+};
+function addNewBook () {
+    //author, title, number of pages, whether it's been read.
+};
+function removeBook () {
+
+};
+function changeReadStatus () {
+
+};
 //SCRIPT---------------------------------------------------------
+    //New book button
+    //Remove book button
+    //Change read status button
