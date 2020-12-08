c43072bf1454d16254d4702b42aa71c92d5609e1
adyang94 <adyang94@yahoo.com>
2020-12-08 11:40:36 -0600

    updates

--- library/library.js
+++ library/library.js
@@ -1,18 +1,22 @@
 //CONSTANTS AND VARIABLES----------------------------------------
-let myLibrary = [];
+let myLibrary = [
+    {
+        title: "Hello World", //example
+    }
+];
 
 const bookCards = document.querySelector('.cards');
 const newBookButton = document.querySelector('#newBookButton');
-const authorInput = document.querySelector('#author');
-const numberOfPagesInput = document.querySelector('#numberOfPages');
+const formAuthor = document.querySelector('#formAuthor');
+const formNumberOfPages = document.querySelector('#formNumberOfPages');
+const formTitle = document.querySelector('#formTitle');
 
 //FUNCTIONS------------------------------------------------------
-function book(title, author, numberOfPages, read) {
+function book(title, author, numberOfPages) {
     //the object constructor
     this.title = title;
     this.author = author;
     this.numberOfPages = numberOfPages;
-    this.read = read;
     //send info to the displayBooks function to be displayed.
 };
 function addBookToLibrary () {
@@ -22,33 +26,44 @@ function displayBooks () {
     //Will need some example books to display.
     //displayBooks iterates through every single book to display it.
     const books = document.querySelectorAll('.book');
+    // Remove old cards
     books.forEach(book => displayBooks.removeChild(book));
-
+    console.log(`Library Length: ${myLibrary.length}`);
+    //Create new cards
     for(i=0; i < myLibrary.length; i++) {
         //create elements for the card.
-        const library = document.querySelector('.libraryContainer');
+        const library = document.querySelector('.library');
         const bookDiv = document.createElement('div');
         const titleDiv = document.createElement('div');
         const authorDiv = document.createElement('div');
         const numberOfPagesDiv = document.createElement('div');
-        const cardCheckbox = document.createElement('button');
-        //append elements for the card to library container.
+        const removeButton = document.createElement('button');
+        const checkboxDiv = document.createElement('div');
+        const cardCheckbox = document.createElement('input');
+            cardCheckbox.setAttribute("type", "checkbox")
+        //append elements to library.
         library.appendChild(bookDiv);
         bookDiv.appendChild(titleDiv)
         bookDiv.appendChild(authorDiv);
         bookDiv.appendChild(numberOfPagesDiv);
-        bookDiv.appendChild(cardCheckbox)
-        //add information/text to appropriate areas.
+        bookDiv.appendChild(removeButton);
+        
+        bookDiv.appendChild(checkboxDiv);
+        checkboxDiv.appendChild(cardCheckbox);
+
+        //add information/text
         titleDiv.textContent = myLibrary[i].title;
         authorDiv.textContent = myLibrary[i].author;
         numberOfPagesDiv.textContent = myLibrary[i].numberOfPages;
-        //add classes to book cards for CSS
+        removeButton.textContent = "Remove";
+        checkboxDiv.textContent = "Read??"
+        //add classes to card divs for CSS
+        bookDiv.classList.add('bookDiv');
         titleDiv.classList.add('title');
         authorDiv.classList.add('author');
         numberOfPagesDiv.classList.add('numberOfPages');
         cardCheckbox.classList.add('cardCheckbox');
-        //read button toggle
-
+        removeButton.classList.add('removeButton');
     }
 };
 function addNewBook () {
@@ -63,7 +78,7 @@ function addNewBook () {
 };
 function removeBook () {
 
-};l
+};
 function changeReadStatus () {
     //Needs to be able to toggle the book's read status on the book's prototype instance
     console.log('FUNCTION CALLED: changeReadStatus');
@@ -73,6 +88,6 @@ function changeReadStatus () {
 //SCRIPT---------------------------------------------------------
 //New book button
 newBookButton.addEventListener('click', addNewBook);
-readButton.addEventListener('click', changeReadStatus);
+displayBooks();
 //Remove book button
 //Change read status button
