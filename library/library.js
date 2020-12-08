5b1aae0c6365c2f07ec28186dda92c685c52eed2
adyang94 <adyang94@yahoo.com>
2020-12-08 09:07:12 -0600

    updates

--- library/library.js
+++ library/library.js
@@ -8,20 +8,30 @@ const numberOfPagesInput = document.querySelector('#numberOfPages');
 const readButton = document.querySelector('#read');
 
 //FUNCTIONS------------------------------------------------------
-function book(author, numberOfPages, read) {
+function book(title, author, numberOfPages, read) {
     //the object constructor
+    this.title = title;
     this.author = author;
     this.numberOfPages = numberOfPages;
     this.read = read;
+    //send info to the displayBooks function to be displayed.
 };
 function addBookToLibrary () {
     //May not be needed.
 };
 function displayBooks () {
     //Will need some example books to display.
+    //displayBooks iterates through every single book to display it.
+    const books = document.querySelectorAll('.book');
+    books.forEach(book => displayBooks.removeChild(book));
+
+    for(i=0; i < myLibrary.length; i++) {
+
+    }
 };
 function addNewBook () {
     //Get info from input and send to book constructor.
+    let newBook = new book(author, )
     //Send book constructor and library array to displayBooks function
 
     //author, title, number of pages, whether it's been read.
@@ -30,7 +40,7 @@ function addNewBook () {
 };
 function removeBook () {
 
-};
+};l
 function changeReadStatus () {
     //Needs to be able to toggle the book's read status on the book's prototype instance
     console.log('FUNCTION CALLED: changeReadStatus');
