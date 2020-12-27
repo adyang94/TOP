976dfc9b6f984b2a50ee56d1ec7a5b6961cd5eb6
adyang94 <adyang94@yahoo.com>
2020-12-26 22:14:28 -0600

    update

--- restaurant-project/src/index.js
+++ restaurant-project/src/index.js
@@ -10,24 +10,30 @@ const framework = (() => {
     let title = document.createElement('title');
     title.innerHTML = "Eat Moo Chicken Foo!";
     htmlHead.appendChild(title);
-
+    
     let img = document.createElement('img');
     img.src = "../pictures/Chick-fil-A-logo.jpeg";
     content.appendChild(img);
     console.log('1');
-    
-    function renderHomepage () {
-        //function to erase content and call homepage module
-        //import homepage module
-        homepageFramework();
-    }
+    tabs.renderHomepage();
     
     function reset() {
         //function to erase page current page.
     }
-
+    
     //Tabs
     
+})()
+const tabs = (() => {
+    function renderHomepage () {
+        //function to erase content and call homepage module
+        //import homepage module
+        homepageFramework();
+        console.log('HELLO2');
+    }
+    return {
+        renderHomepage
+    }
 })()
 
 //SCRIPT---------------------------------------------------------
