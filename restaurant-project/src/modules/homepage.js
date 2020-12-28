2b3ffeaaae5becb375a4463e29f754049d880c8f
adyang94 <adyang94@yahoo.com>
2020-12-27 22:50:48 -0600

    updates

--- restaurant-project/src/modules/homepage.js
+++ restaurant-project/src/modules/homepage.js
@@ -1,9 +1,10 @@
 import {content} from '../index';
 console.log('HOMEPAGE MODULE RUNNING');
 export function homepageFramework () {
-    // let content = document.querySelector('#content');
+    //Introduction
     let Introduction = document.createElement('h1');
-    Introduction.innerHTML = 'Welcome to Thai Thani!';
+    Introduction.classList.add('homepageIntroduction');
+    Introduction.innerHTML = 'Welcome!';
     content.appendChild(Introduction);
     
 }
