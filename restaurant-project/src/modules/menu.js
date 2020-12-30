1c14905b94af18d6ac9c2d86489e5d4ebf3df191
adyang94 <adyang94@yahoo.com>
2020-12-29 19:42:11 -0600

    updates

--- restaurant-project/src/modules/menu.js
+++ restaurant-project/src/modules/menu.js
@@ -2,17 +2,21 @@ import {content} from '../index';
 export function menuFramework() {
     console.log('menu module working!');
     //appetizers
-    let appetizersTitle = document.createElement('p');
+    let appetizersTitle = document.createElement('h3');
     appetizersTitle.classList.add('menuTitle');
     appetizersTitle.innerHTML = 'Appetizers';
     content.appendChild(appetizersTitle);
 
     let appetizers = document.createElement('p');
-    appetizersTitle.classList.add('appetizers');
+    appetizers.classList.add('appetizers');
     appetizers.innerHTML = 'Chicken Satay <br> Thai Spring Rolls <br> Pot Stickers';
     content.appendChild(appetizers);
 
     //lunch specials
+    let lunchSpecialsTitle = document.createElement('h3');
+    lunchSpecialsTitle.classList.add('menuTitle');
+    lunchSpecialsTitle.innerHTML = 'Lunch Specials';
+    content.appendChild(lunchSpecialsTitle); 
     //noodles
     //stir fry
     // fried rice
