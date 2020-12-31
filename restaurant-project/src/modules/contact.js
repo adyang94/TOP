ee2a6ea0cccecb23e72dbf5c76b19b226ffa455d
adyang94 <adyang94@yahoo.com>
2020-12-31 11:57:54 -0600

    updates

--- restaurant-project/src/modules/contact.js
+++ restaurant-project/src/modules/contact.js
@@ -1,4 +1,16 @@
 import {content} from '../index';
 export function contactFramework() {
     console.log('contact module working');
+    let contactTitle = document.createElement('h3');
+    contactTitle.innerHTML = 'Contact Information:'
+    contactTitle.classList.add('moduleTitle');
+    content.appendChild(contactTitle);
+
+    let contact = document.createElement('p');
+    contact.classList.add('moduleInfo');
+    contact.innerHTML = 'Address: <span>111 Tasty Avenue</span> <br> Phone: <span>867-5309</span>'
+    content.appendChild(contact);
+
+    let contactImg = document.createElement('img');
+    contactImg.src
 }
