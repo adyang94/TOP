92a9e3103aa52250302567c6dda96aebb95b402f
adyang94 <adyang94@yahoo.com>
2020-12-31 14:44:21 -0600

    updates

--- restaurant-project/src/modules/aboutUs.js
+++ restaurant-project/src/modules/aboutUs.js
@@ -1,11 +1,82 @@
 import {content} from '../index';
-export function menuFramework() {
-    console.log('menu module working!');
+export function aboutUsFramework() {
+    console.log('aboutUs module working!');
     
     let tabs = document.createElement('div');
+    tabs.classList.add('tabs');
+    content.appendChild(tabs);
+        // Setting the up the appetizers tab
         let appetizersTab = document.createElement('input');
+        appetizersTab.type = 'radio';
+        appetizersTab.name = 'tabs';
+        appetizersTab.id = 'appetizersTab';
+        appetizersTab.checked = 'checked';
+        // appetizersTab.setAttribute = ("name", "tabs");
+        // appetizersTab.setAttribute = ('id', 'appetizersTab');
+        // appetizersTab.setAttribute = ('checked', 'checked');
+        tabs.appendChild(appetizersTab);
+        
         let appetizersTabLabel = document.createElement('label');
+        appetizersTabLabel.innerHTML = 'Appetizer';
+        appetizersTabLabel.setAttribute('for','appetizersTab');
+        tabs.appendChild(appetizersTabLabel);
+        
         let appetizerContainer = document.createElement('div');
-            let 
+        tabs.appendChild(appetizerContainer);
+        appetizerContainer.classList.add('tab');
+            let appetizerHeader = document.createElement('h3');
+            appetizerHeader.innerHTML = 'Appetizers';
+            appetizerContainer.appendChild(appetizerHeader);
 
+            let appetizers = document.createElement('p');
+            appetizers.innerHTML = 'Chicken Satay <br> Thai Spring Rolls <br> Pot Stickers';
+            appetizerContainer.appendChild(appetizers);
+        //Setting up entrees
+        let entreesTab = document.createElement('input');
+        entreesTab.type = 'radio';
+        entreesTab.name = 'tabs';
+        entreesTab.id = 'entreesTab';
+        entreesTab.checked = 'checked';
+        tabs.appendChild(entreesTab);
+        
+        let entreesTabLabel = document.createElement('label');
+        entreesTabLabel.innerHTML = 'Entrees';
+        tabs.appendChild(entreesTabLabel);
+        entreesTabLabel.setAttribute('for', 'entreesTab');
+        
+        let entreesContainer = document.createElement('div');
+        tabs.appendChild(entreesContainer);
+        entreesContainer.classList.add('tab');
+            let entreesHeader = document.createElement('h3');
+            entreesHeader.innerHTML = 'Entrees';
+            entreesContainer.appendChild(entreesHeader);
+
+            let entrees = document.createElement('p');
+            entrees.innerHTML = 'Drunken Noodles <br> Lo Mein <br> Pad Kee Mao <br> Pad Thai <br> Pad Woon Sen <br> Pho Special Noodle Soup <br> Udon Noodle Soup <br> Seafood Pho <br> Chicken with Broccoli <br> Beef with Broccoli <br> Basil Stir-Fry <br> Beef Pepper Steak <br> Garlic Pepper Stir-Fry <br> Sweet and Sour Chicken <br> Lava Chicken <br> Combo Fried Rice <br> Chicken Fried Rice <br> Beef Fried Rice <br> Pineapple Fried Rice <br> Fried Rice with Basil <br> Vegetable Fried Rice <br> ';
+            entreesContainer.appendChild(entrees);
+        
+        // Setting up drinks tab
+        let drinksTab = document.createElement('input');
+        drinksTab.type = 'radio';
+        drinksTab.name = 'tabs';
+        drinksTab.id = 'drinksTab';
+        drinksTab.checked = 'checked';
+        tabs.appendChild(drinksTab);
+        
+        let drinksTabLabel = document.createElement('label');
+        drinksTabLabel.innerHTML = 'Drinks';
+        tabs.appendChild(drinksTabLabel);
+        
+        drinksTabLabel.setAttribute('for', 'drinksTab');
+        
+        let drinksContainer = document.createElement('div');
+        tabs.appendChild(drinksContainer);
+        drinksContainer.classList.add('tab');
+            let drinksHeader = document.createElement('h3');
+            drinksHeader.innerHTML = 'Drinks';
+            drinksContainer.appendChild(drinksHeader);
+
+            let drinks = document.createElement('p');
+            drinks.innerHTML = 'Thai Iced Tea <br> Thai Iced Coffee <br> Fountain Drinks <br> Hot Tea <br> Bottled Water';
+            drinksContainer.appendChild(drinks);
 }
--- restaurant-project/src/modules/menu.js
+++ restaurant-project/src/modules/menu.js
@@ -1,69 +1,82 @@
 import {content} from '../index';
-export function menuFramework() {
-    console.log('menu module working!');
-   
-    //appetizers
-    let appetizersTitle = document.createElement('h3');
-    appetizersTitle.classList.add('menuTitle');
-    appetizersTitle.innerHTML = 'Appetizers';
-    content.appendChild(appetizersTitle);
-
-    let appetizers = document.createElement('p');
-    appetizers.classList.add('meals');
-    appetizers.innerHTML = 'Chicken Satay <br> Thai Spring Rolls <br> Pot Stickers';
-    content.appendChild(appetizers);
-
-    //lunch specials
-    let lunchSpecialsTitle = document.createElement('h3');
-    lunchSpecialsTitle.classList.add('menuTitle');
-    lunchSpecialsTitle.innerHTML = 'Lunch Specials';
-    content.appendChild(lunchSpecialsTitle); 
-
-    let lunchSpecials = document.createElement('p');
-    lunchSpecials.classList.add('meals');
-    lunchSpecials.innerHTML = 'Cashew Chicken <br> Mongolian Beef <br> Garlic Stir Fry <br> Pad Thai <br> Green Curry <br> Red Curry <br> Fried Rice <br> Tom Yum Chicken Soup';
-    content.appendChild(lunchSpecials);
-
-    //noodles
-    let noodlesTitle = document.createElement('h3');
-    noodlesTitle.classList.add('menuTitle');
-    noodlesTitle.innerHTML = 'Noodles';
-    content.appendChild(noodlesTitle);
-
-    let noodles = document.createElement('p');
-    noodles.classList.add('meals');
-    noodles.innerHTML = 'Drunken Noodles <br> Lo Mein <br> Pad Kee Mao <br> Pad Thai <br> Pad Woon Sen <br> Pho Special Noodle Soup <br> Udon Noodle Soup <br> Seafood Pho';
-    content.appendChild(noodles);
+export function aboutUsFramework() {
+    console.log('aboutUs module working!');
     
-    //stir fry
-    let stirFryTitle = document.createElement('h3');
-    stirFryTitle.classList.add('menuTitle');
-    stirFryTitle.innerHTML = 'Stir Fry';
-    content.appendChild(stirFryTitle);
+    let tabs = document.createElement('div');
+    tabs.classList.add('tabs');
+    content.appendChild(tabs);
+        // Setting the up the appetizers tab
+        let appetizersTab = document.createElement('input');
+        appetizersTab.type = 'radio';
+        appetizersTab.name = 'tabs';
+        appetizersTab.id = 'appetizersTab';
+        appetizersTab.checked = 'checked';
+        // appetizersTab.setAttribute = ("name", "tabs");
+        // appetizersTab.setAttribute = ('id', 'appetizersTab');
+        // appetizersTab.setAttribute = ('checked', 'checked');
+        tabs.appendChild(appetizersTab);
+        
+        let appetizersTabLabel = document.createElement('label');
+        appetizersTabLabel.innerHTML = 'Appetizer';
+        appetizersTabLabel.setAttribute('for','appetizersTab');
+        tabs.appendChild(appetizersTabLabel);
+        
+        let appetizerContainer = document.createElement('div');
+        tabs.appendChild(appetizerContainer);
+        appetizerContainer.classList.add('tab');
+            let appetizerHeader = document.createElement('h3');
+            appetizerHeader.innerHTML = 'Appetizers';
+            appetizerContainer.appendChild(appetizerHeader);
 
-    let stirFry = document.createElement('p');
-    stirFry.classList.add('meals');
-    stirFry.innerHTML = 'Chicken with Broccoli <br> Beef with Broccoli <br> Basil Stir-Fry <br> Beef Pepper Steak <br> Garlic Pepper Stir-Fry <br> Sweet and Sour Chicken <br> Lava Chicken';
-    content.appendChild(stirFry);
-    // fried rice
-    let friedRiceTitle = document.createElement('h3');
-    friedRiceTitle.classList.add('menuTitle');
-    friedRiceTitle.innerHTML = 'Fried Rice';
-    content.appendChild(friedRiceTitle);
+            let appetizers = document.createElement('p');
+            appetizers.innerHTML = 'Chicken Satay <br> Thai Spring Rolls <br> Pot Stickers';
+            appetizerContainer.appendChild(appetizers);
+        //Setting up entrees
+        let entreesTab = document.createElement('input');
+        entreesTab.type = 'radio';
+        entreesTab.name = 'tabs';
+        entreesTab.id = 'entreesTab';
+        entreesTab.checked = 'checked';
+        tabs.appendChild(entreesTab);
+        
+        let entreesTabLabel = document.createElement('label');
+        entreesTabLabel.innerHTML = 'Entrees';
+        tabs.appendChild(entreesTabLabel);
+        entreesTabLabel.setAttribute('for', 'entreesTab');
+        
+        let entreesContainer = document.createElement('div');
+        tabs.appendChild(entreesContainer);
+        entreesContainer.classList.add('tab');
+            let entreesHeader = document.createElement('h3');
+            entreesHeader.innerHTML = 'Entrees';
+            entreesContainer.appendChild(entreesHeader);
 
-    let friedRice = document.createElement('p');
-    friedRice.classList.add('meals');
-    friedRice.innerHTML = 'Combo Fried Rice <br> Chicken Fried Rice <br> Beef Fried Rice <br> Pineapple Fried Rice <br> Fried Rice with Basil <br> Vegetable Fried Rice';
-    content.appendChild(friedRice);
+            let entrees = document.createElement('p');
+            entrees.innerHTML = 'Drunken Noodles <br> Lo Mein <br> Pad Kee Mao <br> Pad Thai <br> Pad Woon Sen <br> Pho Special Noodle Soup <br> Udon Noodle Soup <br> Seafood Pho <br> Chicken with Broccoli <br> Beef with Broccoli <br> Basil Stir-Fry <br> Beef Pepper Steak <br> Garlic Pepper Stir-Fry <br> Sweet and Sour Chicken <br> Lava Chicken <br> Combo Fried Rice <br> Chicken Fried Rice <br> Beef Fried Rice <br> Pineapple Fried Rice <br> Fried Rice with Basil <br> Vegetable Fried Rice <br> ';
+            entreesContainer.appendChild(entrees);
+        
+        // Setting up drinks tab
+        let drinksTab = document.createElement('input');
+        drinksTab.type = 'radio';
+        drinksTab.name = 'tabs';
+        drinksTab.id = 'drinksTab';
+        drinksTab.checked = 'checked';
+        tabs.appendChild(drinksTab);
+        
+        let drinksTabLabel = document.createElement('label');
+        drinksTabLabel.innerHTML = 'Drinks';
+        tabs.appendChild(drinksTabLabel);
+        
+        drinksTabLabel.setAttribute('for', 'drinksTab');
+        
+        let drinksContainer = document.createElement('div');
+        tabs.appendChild(drinksContainer);
+        drinksContainer.classList.add('tab');
+            let drinksHeader = document.createElement('h3');
+            drinksHeader.innerHTML = 'Drinks';
+            drinksContainer.appendChild(drinksHeader);
 
-    // drinks 
-    let drinksTitle = document.createElement('h3');
-    drinksTitle.classList.add('menuTitle');
-    drinksTitle.innerHTML = 'Drinks';
-    content.appendChild(drinksTitle);
-
-    let drinks = document.createElement('p');
-    drinks.classList.add('meals');
-    drinks.innerHTML = 'Thai Iced Tea <br> Thai Iced Coffee <br> Fountain Drinks <br> Hot Tea <br> Bottled Water';
-    content.appendChild(drinks);
+            let drinks = document.createElement('p');
+            drinks.innerHTML = 'Thai Iced Tea <br> Thai Iced Coffee <br> Fountain Drinks <br> Hot Tea <br> Bottled Water';
+            drinksContainer.appendChild(drinks);
 }
