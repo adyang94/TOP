159125ac1a65ea6bf4260f579e03ed201699adae
adyang94 <adyang94@yahoo.com>
2021-01-25 19:22:01 -0600

    webpack asset management lesson

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,12 +1,20 @@
 import _ from 'lodash';
 import myName from './myName';
+import './style.css';
+import Icon from './icon.jpg';
 
 function component () {
     const element = document.createElement('div');
 
     //Lodash is now imported by this script:
     element.innerHTML = myName('ady');
+    element.classList.add('hello');
     
+    // Add the image to existing div
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
     
     return element;
 }
