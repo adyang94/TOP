338401ad70ec74f13823517bf439485f5c32e7f9
adyang94 <adyang94@yahoo.com>
2021-01-25 19:52:51 -0600

    webpack asset management lesson

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -2,6 +2,8 @@ import _ from 'lodash';
 import myName from './myName';
 import './style.css';
 import Icon from './icon.jpg';
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component () {
     const element = document.createElement('div');
@@ -14,7 +16,10 @@ function component () {
     const myIcon = new Image();
     myIcon.src = Icon;
 
-    element.appendChild(myIcon);
+    element.appendChild(myIcon); 
+
+    console.log(Data);
+    console.log(Notes);
     
     return element;
 }
