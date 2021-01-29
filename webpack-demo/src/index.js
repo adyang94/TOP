8514d17b1c240c6b656effcc5135e1980512faae
adyang94 <adyang94@yahoo.com>
2021-01-28 20:05:08 -0600

    async lesson

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -5,7 +5,6 @@ function component () {
     const element = document.createElement('div');
     const btn = document.createElement('button');
 
-    element.innerHTML = myName('ady');
     btn.innerHTML = 'Click me and check the console!';
     btn.onclick = printMe;
 
