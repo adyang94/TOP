86b950409c36b68b724f81478b3e81bf5cc91680
adyang94 <adyang94@yahoo.com>
2021-02-19 14:04:23 -0600

    learning React

---
+++ task-app/src/App.js
@@ -0,0 +1,10 @@
+// App.js will handle the input field with the logic.
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
---
+++ task-app/src/Components/Overview.js
@@ -0,0 +1,1 @@
+// Overview will render our tasks.
