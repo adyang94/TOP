0e212c6ac3369c01bf7e39941b8b5cfaa90816ee
adyang94 <adyang94@yahoo.com>
2021-03-01 16:48:44 -0600

    react handles and rendering lists

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,10 +1,60 @@
-// App.js will handle the input field with the logic.
-function App() {
-  return (
-    <div className="App">
+import React, { Component } from "react";
+import Overview from "./Components/Overview.js";
 
-    </div>
-  );
+// App.js will handle the input field with the logic.
+
+class App extends Component {
+  constructor() {
+    super();
+    
+    this.state = { // Defining state.
+      task: "",
+      tasks: [],
+    };
+  }
+
+  callbackFunction = (message) => {
+    alert(message);
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      task: e.target.value,
+    });
+  };
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: "",
+    });
+    console.log(`Tasks: ${this.state.tasks}`);
+  };
+
+  render () {
+    const {task, tasks} = this.state; // Destructuring state.
+
+    return (
+      <div>
+        <form onSubmit={this.onSubmitTask}>
+          <label htmlFor="taskInput">Enter task</label>
+          <input 
+            type="text"
+            id="taskInput"
+            onChange = {this.handleChange}
+            value = {this.state.task}
+          />
+          <button type="submit">
+            Add Task
+          </button>
+        </form>
+        <Overview 
+          tasks = {tasks}
+          callback = {this.callbackFunction}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- task-app/src/Components/Overview.js
+++ task-app/src/Components/Overview.js
@@ -1,1 +1,30 @@
 // Overview will render our tasks.
+
+import React from "react";
+import uniqid from "uniqid";
+
+const removeItem = (tasks) => {
+  const {callback} = this.props.callback;
+  callback('hi');
+}
+
+const Overview = (props) => {
+  const {tasks} = props;
+
+  return (
+    <div>
+      <ul>
+        {tasks.map((task) => {
+          return (
+            <div>
+              <li class = {uniqid()} key = {uniqid()}>{task}</li>
+              <button onClick = {this.removeItem}>Delete</button>
+            </div>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default Overview;
